fa8ca8cbb8120da4dff2c4e6f14c35cc06aa2591
Muhammad Saad Haleem <49789953+MuhammadSaadHaleem@users.noreply.github.com>
2023-11-21 00:33:27 +0500

    Update index.js

--- olx-backend/routes/index.js
+++ olx-backend/routes/index.js
@@ -1,15 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-//localhost:3000/ads
-router.use('/ads', require('./ads'))
 
-//localhost:3000/users
-router.use('/users', require('./user'))
-
-//localhost:3000/olxAds
 router.use('/olx', require('./olxAds'))
-
 
 
 module.exports = router
